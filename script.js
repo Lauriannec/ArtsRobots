6cc408915bd34ff1b05d0efec825651a8222b688
Lauriannec <34099056+Lauriannec@users.noreply.github.com>
2018-09-20 15:12:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toggleImage(id) {
-    var image = document.getElementById(id);
-    image.style.display = image.style.display == "none" ? "block" : "none";
+  var image = document.getElementById(id);
+  image.style.display = image.style.display == "none" ? "block" : "none";
 }
 
 function toggleBoth() {
-  toggleImage('image1')
-  toggleImage('image2')
+  toggleImage("image1");
+  toggleImage("image2");
 }
